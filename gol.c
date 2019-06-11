8b8ef467f5d86b7d3c67a73a3fde13807d421e98
Jose Manuel <josem.rodriguez.caro@gmail.com>
2019-06-11 22:40:30 +0200

    Memoria dinámica I: Reserva dinámica de memoria para el mundo

--- gol.c
+++ gol.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "gol.h"
 
-/*
- * Al eliminar de la cabecera "gol.h" estas funciones y declararlas como "static" en el fichero "gol.c"
- * consigo invisibilizar para el desarrollador que usa mi librería "gol" estas funciones. Oculto estas 
- * funciones para el usuario de mi librería, o desde una perspectiva de objeto, privatizo estos métodos
- * El linker no es capaz de encontrar referencia a ellas si son llamadas en main.c o cualquier otra parte
- * del código que no sea en este fichero
- */
-static int  count_neighbors(const struct gol *self, int i, int j);  // Cuenta el numero de vecinas vivas de una celda
-static bool get_cell(const struct gol *self, int i, int j); // Obtiene el estado de la celda
+enum world_type {CURRENT_WORLD, NEXT_WORLD};
+
+static int  count_neighbors(struct gol *self, int i, int j);  // Cuenta el numero de vecinas vivas de una celda
+static bool get_cell(struct gol *self, int i, int j); // Obtiene el estado de la celda
+
+bool gol_alloc(struct gol *self, int size_x, int size_y) {
+
+    for(int world = CURRENT_WORLD; world <= NEXT_WORLD; world++) {
+
+        self->worlds[world] = malloc(size_x * sizeof(bool *)); // Reserva dinámica de vector de punteros a fila (bool *)
+
+        if (!self->worlds[world]) {
+            return false;
+        }
+
+        for (int row = 0; row < size_x; row++) {
+            self->worlds[world][row] = malloc(size_y * sizeof(bool)); // Reserva dinámica de cada fila
+
+        if (!self->worlds[world][row])
+                return false;
+        }
+    }
+
+    self->size_x = size_x;
+    self->size_y = size_y;
+
+    return true;
+}
+
+void gol_free(struct gol *self) {
+
+    for (int world = CURRENT_WORLD; world <= NEXT_WORLD; world++) {
+        for (int row=0; row < self->size_x; row++)
+            free(self->worlds[world][row]);
+        free(self->worlds[world]);
+    }
+}
 
 void gol_init(struct gol *self)
 {
-    // Poner el mundo a false
 
-    for (bool *ini = self->world_a[0]; ini < self->world_a[0] + TAM_X*TAM_Y; ini++)
-
-        *ini = false;
+    for (int i = 0; i < self->size_x; i++)
+        for (int j = 0; j < self->size_y; j++)
+            self->worlds[CURRENT_WORLD][i][j] = false;
 
     /* Inicializar con el patrón del glider:
     *           . # .
@@ -25,17 +53,14 @@ void gol_init(struct gol *self)
     *           # # #
     */
 
-    *(self->world_a[0] + 1 + 0*TAM_Y) = true;
-    *(self->world_a[0] + 2 + 1*TAM_Y) = true;
-    *(self->world_a[0] + 0 + 2*TAM_Y) = true;
-    *(self->world_a[0] + 1 + 2*TAM_Y) = true;
-    *(self->world_a[0] + 2 + 2*TAM_Y) = true;
-
-    self->current = self->world_a[0];
-    self->next = self->world_b[0];
+    self->worlds[CURRENT_WORLD][0][1] = true;
+    self->worlds[CURRENT_WORLD][1][2] = true;
+    self->worlds[CURRENT_WORLD][2][0] = true;
+    self->worlds[CURRENT_WORLD][2][1] = true;
+    self->worlds[CURRENT_WORLD][2][2] = true;
 }
 
-void gol_print(const struct gol *self)
+void gol_print(struct gol *self)
 {
     // Imprimir el mundo por consola. Sugerencia:
     /*
@@ -57,7 +82,7 @@ void gol_print(const struct gol *self)
         for (int j = 0; j < TAM_Y; j++)
         {
 
-            printf(" %c", *(self->current + j + i*TAM_Y) ? '#' : '.');
+            printf(" %c", self->worlds[CURRENT_WORLD][i][j] ? '#' : '.');
         }
 
         printf("\n");
@@ -78,33 +103,33 @@ void gol_step(struct gol *self)
 
     int alives = 0;
 
-    for (int i = 0; i < TAM_X; i++)
+    for (int i = 0; i < self->size_x; i++)
     {
 
-        for (int j = 0; j < TAM_Y; j++)
+        for (int j = 0; j < self->size_y; j++)
         {
 
             alives = count_neighbors(self, i, j);
 
             if(get_cell(self, i, j))
 
-                *(self->next + j + i*TAM_Y) = alives == 2 || alives == 3;
+                self->worlds[NEXT_WORLD][i][j] = alives == 2 || alives == 3;
 
             else
 
-                *(self->next + j + i*TAM_Y) = alives == 3;
+                self->worlds[NEXT_WORLD][i][j] = alives == 3;
         }
 
         alives = 0;
     }
 
     // Intercambiamos mundos
-    bool *tmp_world_p = self->current;
-    self->current = self->next;
-    self->next = tmp_world_p;
+    bool **swap = self->worlds[CURRENT_WORLD];
+    self->worlds[CURRENT_WORLD] = self->worlds[NEXT_WORLD];
+    self->worlds[NEXT_WORLD] = swap;
 }
 
-static int count_neighbors(const struct gol *self, int i, int j)
+int count_neighbors(struct gol *self, int i, int j)
 {
     // Devuelve el número de vecinos
     int counter = 0;
@@ -122,16 +147,17 @@ static int count_neighbors(const struct gol *self, int i, int j)
 
 }
 
-static bool get_cell(const struct gol *self, int i, int j)
+bool get_cell(struct gol *self, int i, int j)
 {
     /*
     * Devuelve el estado de la célula de posición indicada
     * (¡cuidado con los límites del array!)
     */
 
-    if ( i < 0 || i >= TAM_X || j < 0 || j >= TAM_Y)
 
+    if ( i < 0 || i >= TAM_X || j < 0 || j >= TAM_Y)
         return 0;
 
-    return *(self->current + j + i*TAM_Y);
+    return self->worlds[CURRENT_WORLD][i][j];
 }
+
--- main.c
+++ main.c
@@ -5,29 +5,27 @@
 int main()
 {
     int i = 0;
-
-    // Reserva estática de memoria: el compilador reserva espacio en la memoria
-    // en la declaración del tipo struct gol. Conoce el espacio porque conoce
-    // la definición de la estructura (el tipo) por la cabecera.
     struct gol g;
 
-    // inicializa el mundo
+
+    if(!gol_alloc(&g, TAM_X, TAM_Y)) {
+
+        fprintf(stderr, "Error: no se ha podido reservar memoria\n");
+        exit(EXIT_FAILURE);
+    }
+
     gol_init(&g);
 
     do
     {
         printf("\033cIteration %d\n", i++);
-        // Imprime el mundo
-        gol_print(&g);
 
-        // Itera
-        // Paso por referencia: las estructuras pueden copiarse por valor, pero
-        // se usa el paso por referencia porque si la estructura contiene muchos
-        // campos, la copia desperdicia memoria.
+        gol_print(&g);
         gol_step(&g);
 
-    }
-    while (getchar() != 'q');
+    } while (getchar() != 'q');
+
+    gol_free(&g);
 
     return EXIT_SUCCESS;
 }
